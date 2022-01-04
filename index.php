0e62e4c3434c2732e8f639a0e8ff6be2b465aa97
sebabrata <sebabratakundu9@gmail.com>
2022-01-04 13:34:01 +0530

    fix incorrect tag

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 		
 		<div class="container mt-3 mb-5">
 			<div class="jumbotron" id="services">
-				<h3 class="font-gothic text-center mb-3">OSMS SERVICES</h4>
+				<h4 class="font-gothic text-center mb-3">OSMS SERVICES</h4>
 				<p class="text-justify">industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
 			</div>
 		</div>
